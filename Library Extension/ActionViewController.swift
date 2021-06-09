0c956abcb3c8cde4026ba18fb63a8913432f676a
Zachary Lazzara <lazzarazachary@gmail.com>
2021-06-09 13:09:08 -0400

    Figuring out storyboards for the extension.

--- Library Extension/ActionViewController.swift
+++ Library Extension/ActionViewController.swift
@@ -7,15 +7,38 @@
 
 import UIKit
 import MobileCoreServices
+import PDFKit
 
 class ActionViewController: UIViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
+    //@IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pdfView: PDFView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        
+        var pdfFound = false
+        for item in self.extensionContext!.inputItems as! [NSExtensionItem] {
+            for provider in item.attachments! {
+                if provider.hasItemConformingToTypeIdentifier(kUTTypePDF as String) {
+                    weak var weakPDFView = self.pdfView
+                    provider.loadItem(forTypeIdentifier: kUTTypePDF as String, options: nil, completionHandler: { (pdfURL, error) in
+                        OperationQueue.main.addOperation {
+                            if let strongPDFView = weakPDFView {
+                                if let pdfURL = pdfURL as? URL {
+                                    strongPDFView.document = PDFDocument(url: pdfURL)
+                                }
+                            }
+                        }
+                    })
+                    pdfFound = true
+                    break
+                }
+            }
+            if (pdfFound) {
+                break
+            }
+        }
         
         
         
@@ -26,32 +49,32 @@ class ActionViewController: UIViewController {
         
         // For example, look for an image and place it into an image view.
         // Replace this with something appropriate for the type[s] your extension supports.
-        var imageFound = false
-        for item in self.extensionContext!.inputItems as! [NSExtensionItem] {
-            for provider in item.attachments! {
-                if provider.hasItemConformingToTypeIdentifier(kUTTypeImage as String) {
-                    // This is an image. We'll load it, then place it in our image view.
-                    weak var weakImageView = self.imageView
-                    provider.loadItem(forTypeIdentifier: kUTTypeImage as String, options: nil, completionHandler: { (imageURL, error) in
-                        OperationQueue.main.addOperation {
-                            if let strongImageView = weakImageView {
-                                if let imageURL = imageURL as? URL {
-                                    strongImageView.image = UIImage(data: try! Data(contentsOf: imageURL))
-                                }
-                            }
-                        }
-                    })
-                    
-                    imageFound = true
-                    break
-                }
-            }
-            
-            if (imageFound) {
-                // We only handle one image, so stop looking for more.
-                break
-            }
-        }
+//        var imageFound = false
+//        for item in self.extensionContext!.inputItems as! [NSExtensionItem] {
+//            for provider in item.attachments! {
+//                if provider.hasItemConformingToTypeIdentifier(kUTTypeImage as String) {
+//                    // This is an image. We'll load it, then place it in our image view.
+//                    weak var weakImageView = self.imageView
+//                    provider.loadItem(forTypeIdentifier: kUTTypeImage as String, options: nil, completionHandler: { (imageURL, error) in
+//                        OperationQueue.main.addOperation {
+//                            if let strongImageView = weakImageView {
+//                                if let imageURL = imageURL as? URL {
+//                                    strongImageView.image = UIImage(data: try! Data(contentsOf: imageURL))
+//                                }
+//                            }
+//                        }
+//                    })
+//
+//                    imageFound = true
+//                    break
+//                }
+//            }
+//
+//            if (imageFound) {
+//                // We only handle one image, so stop looking for more.
+//                break
+//            }
+//        }
     }
 
     @IBAction func done() {
