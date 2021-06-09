12e7588f935ba71737bca45e823a65bc874d0ca7
Zachary Lazzara <lazzarazachary@gmail.com>
2021-06-09 13:16:57 -0400

    Display PDF in extension popup (need to now ask if the user wants to save it, and to which directory or if they want to create a new directory, etc)

--- Library Extension/ActionViewController.swift
+++ Library Extension/ActionViewController.swift
@@ -27,6 +27,7 @@ class ActionViewController: UIViewController {
                             if let strongPDFView = weakPDFView {
                                 if let pdfURL = pdfURL as? URL {
                                     strongPDFView.document = PDFDocument(url: pdfURL)
+                                    strongPDFView.autoScales = true
                                 }
                             }
                         }
@@ -39,42 +40,6 @@ class ActionViewController: UIViewController {
                 break
             }
         }
-        
-        
-        
-        
-        
-        
-        // Get the item[s] we're handling from the extension context.
-        
-        // For example, look for an image and place it into an image view.
-        // Replace this with something appropriate for the type[s] your extension supports.
-//        var imageFound = false
-//        for item in self.extensionContext!.inputItems as! [NSExtensionItem] {
-//            for provider in item.attachments! {
-//                if provider.hasItemConformingToTypeIdentifier(kUTTypeImage as String) {
-//                    // This is an image. We'll load it, then place it in our image view.
-//                    weak var weakImageView = self.imageView
-//                    provider.loadItem(forTypeIdentifier: kUTTypeImage as String, options: nil, completionHandler: { (imageURL, error) in
-//                        OperationQueue.main.addOperation {
-//                            if let strongImageView = weakImageView {
-//                                if let imageURL = imageURL as? URL {
-//                                    strongImageView.image = UIImage(data: try! Data(contentsOf: imageURL))
-//                                }
-//                            }
-//                        }
-//                    })
-//
-//                    imageFound = true
-//                    break
-//                }
-//            }
-//
-//            if (imageFound) {
-//                // We only handle one image, so stop looking for more.
-//                break
-//            }
-//        }
     }
 
     @IBAction func done() {
