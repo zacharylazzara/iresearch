e04b6da02b41c2781bd3042d9475af66c0f03074
zacharylazzara <lazzarazachary@gmail.com>
2021-06-10 09:53:25 -0400

    Looks like were able to use the AppGroup now; need to try loading data from it still.

--- Research Assistant/Views/DirectoryView.swift
+++ Research Assistant/Views/DirectoryView.swift
@@ -34,8 +34,11 @@ struct DirectoryView: View {
         
         do {
             //file:///Users/zachary/Library/Developer/CoreSimulator/Devices/46ACAD62-8A28-46AA-9F3E-F1DDE0A47E3C/data/Containers/Shared/AppGroup/6B55B45C-48D3-457C-B1DA-A2088F35F574//
-            items = try fm.contentsOfDirectory(atPath: AppGroup.library.containerURL.relativeString)
-
+            let dir = fm.containerURL(forSecurityApplicationGroupIdentifier: AppGroup.library.containerURL.path)!.path// + "/Library"
+            print(dir)
+            items = try fm.contentsOfDirectory(atPath: dir)
+            print(items)
+            
             for item in items {
                 print("Found \(item)")
             }
